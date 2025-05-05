b26483e71b82bcbaf8713be01b79f8a9f9bcd7ea
Kushagra Singh Bisen <kushbisen@proton.me>
2025-05-05 14:03:14 +0200

    updated the server to use reusetokenfetcher

--- src/server/HTTPServer.ts
+++ src/server/HTTPServer.ts
@@ -6,6 +6,7 @@ import { WebSocketHandler } from "./WebSocketHandler";
 import * as websocket from 'websocket';
 const EventEmitter = require('events');
 import { TokenManagerService } from "../service/authorization/TokenManager";
+import { ReuseTokenUMAFetcher } from "../service/authorization/ReuseTokenUMAFetcher";
 const token_manager = TokenManagerService.getInstance();
 // const { access_token, token_type } = token_manager.getAccessToken()
 /**
@@ -19,6 +20,7 @@ export class HTTPServer {
     public dynamic_endpoints: { [key: string]: boolean };
     public query_registry: any;
     public websocket_server: any;
+    public uma_fetcher: any;
     public aggregation_publisher: any;
     public websocket_handler: any;
     public event_emitter: any;
@@ -32,6 +34,10 @@ export class HTTPServer {
     constructor(http_port: number, solid_server_url: string, logger: any) {
         this.solid_server_url = solid_server_url;
         this.dynamic_endpoints = {};
+        this.uma_fetcher = new ReuseTokenUMAFetcher({
+            token: "http://n063-04b.wall2.ilabt.iminds.be/replayer#me",
+            token_format: "urn:solidlab:uma:claims:formats:webid"
+        });
         this.http_server = createServer(this.request_handler.bind(this)).listen(http_port);
         this.logger = logger;
         this.websocket_server = new websocket.server({
@@ -74,8 +80,8 @@ export class HTTPServer {
                     const webhook_notification_data = JSON.parse(body);
                     console.log(`Webhook notification data: ${JSON.stringify(webhook_notification_data)}`);
                     this.logger.info({}, 'webhook_notification_data_received');
-                    
-                    
+
+
                     if (webhook_notification_data.type === 'Add') {
                         this.logger.info({}, 'webhook_notification_received');
                         // the target is where a new notification is added into the ldes stream.
@@ -86,24 +92,36 @@ export class HTTPServer {
                         const added_event_location = webhook_notification_data.object;
 
                         const derived_target = this.toDerivedTarget(location_where_event_is_added);
-                        const token_data = await TokenManagerService.getInstance().getAccessToken(derived_target, 'GET');
-                        if (token_data) {
-                            const { access_token, token_type } = token_data;
-                            const latest_event_response = await fetch(derived_target, {
-                                method: 'GET',
-                                headers: {
-                                    'Authorization': `${token_type} ${access_token}`, // Add the access token to the headers.
-                                    'Accept': 'text/turtle'
-                                }
-                            });
-                            const latest_event = await latest_event_response.text();
-                            console.log(`The latest event is ${latest_event}`);
-                            this.event_emitter.emit(`${ldes_stream_where_event_is_added}`, latest_event);
-                            this.logger.info({}, 'webhook_notification_processed_and_emitted');
-                        }
-                        else {
-                            this.logger.error({}, 'webhook_notification_processing_failed as the token is not set for the user and the resource which is empty');
-                        }
+
+                        const latest_event_response = await this.uma_fetcher.fetch({
+                            method: 'GET',
+                            headers: {
+                                'Accept': 'text/turtle'
+                            }
+                        });
+
+                        const latest_event = await latest_event_response.text();
+                        console.log(`The latest event is ${latest_event}`);
+                        this.event_emitter.emit(`${ldes_stream_where_event_is_added}`, latest_event);
+                        this.logger.info({}, 'webhook_notification_processed_and_emitted');
+                        // const token_data = await TokenManagerService.getInstance().getAccessToken(derived_target, 'GET');
+                        // if (token_data) {
+                        //     const { access_token, token_type } = token_data;
+                        //     const latest_event_response = await fetch(derived_target, {
+                        //         method: 'GET',
+                        //         headers: {
+                        //             'Authorization': `${token_type} ${access_token}`, // Add the access token to the headers.
+                        //             'Accept': 'text/turtle'
+                        //         }
+                        //     });
+                        //     const latest_event = await latest_event_response.text();
+                        //     console.log(`The latest event is ${latest_event}`);
+                        //     this.event_emitter.emit(`${ldes_stream_where_event_is_added}`, latest_event);
+                        //     this.logger.info({}, 'webhook_notification_processed_and_emitted');
+                        // }
+                        // else {
+                        //     this.logger.error({}, 'webhook_notification_processing_failed as the token is not set for the user and the resource which is empty');
+                        // }
                     }
                 });
                 break;
@@ -135,10 +153,10 @@ export class HTTPServer {
     public toDerivedTarget(originalUrl: string): string {
         const url = new URL(originalUrl);
         const parts = url.pathname.split('/').filter(Boolean); // removes empty segments
-    
+
         const basePath = parts.slice(0, -1).join('/');  // e.g., "alice"
         const lastSegment = parts[parts.length - 1];    // e.g., "acc-x"
-    
+
         // Construct new path: /alice/derived/acc-x
         url.pathname = `/${basePath}/derived/${lastSegment}`;
         return url.toString();
